580befeafe1656960d87ba7f2fdb05c20e2affeb
Jayjay Ntshwenyese <jayjay@globalkinetic.com>
2021-08-03 14:13:07 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Google Autocomplete Android
---
+++ app/src/main/java/com/ikhiloyaimokhai/googleplacestutorial/AutocompleteFromFragmentActivity.java
@@ -0,0 +1,59 @@
+package com.jayjay.googleplacestutorial;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
+
+public class AutocompleteFromFragmentActivity extends AppCompatActivity {
+
+    private static String TAG = AutocompleteFromFragmentActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autocomplete_from_fragment);
+        Button nextButton = findViewById(R.id.nextButton);
+        String apiKey = getString(R.string.api_key);
+
+        /**
+         * Initialize Places. For simplicity, the API key is hard-coded. In a production
+         * environment we recommend using a secure mechanism to manage API keys.
+         */
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), apiKey);
+        }
+
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+        nextButton.setOnClickListener(v -> startActivity(new Intent(AutocompleteFromFragmentActivity.this, AutocompleteFromIntentActivity.class)));
+    }
+}
